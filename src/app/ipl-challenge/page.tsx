ab26c6e5e713b3ba631b29e270d2fd525a4ab8ef
BoTGoDD <workprofile2305@gmail.com>
2026-09-03 21:52:58 +0530

    Email confirmation changes

---
+++ src/app/ipl-challenge/page.tsx
@@ -0,0 +1,9 @@
+export default function IPLChallengePage() {
+  return (
+    <main className="flex flex-1 items-center justify-center">
+      <h1 className="text-3xl font-black">
+        IPL Challenge
+      </h1>
+    </main>
+  );
+}
